e25b5dda63f9f94d4dbead54924d2394d772d2c7
payas-kumar <bhrigu.payas10@gmail.com>
2022-09-24 23:49:30 +0530

    Added Movie Information and Logo - Parallax included

--- src/Components/featured/featured.jsx
+++ src/Components/featured/featured.jsx
@@ -4,15 +4,21 @@ import { PlayArrowOutlined,InfoOutlined } from "@material-ui/icons"
 const Featured = () => {
     return (
         <div className="featured">
-            <img width={"100%"} src="https://picsum.photos/500"/>
-            <div className="options">
-                <div>
-                    <PlayArrowOutlined className="icon" />
-                    <span className="text">Play</span>
+            <div className="poster"></div>
+            <div className="movie">
+                <img className="logo" src={require("../../images/lordoftherings2.png")}/>
+                <div className="movieInfo">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores temporibus id laboriosam repellendus unde doloremque quasi? Numquam fuga quae accusantium adipisci similique ipsum autem, nesciunt eligendi doloremque possimus consequuntur quidem.
                 </div>
-                <div>
-                    <InfoOutlined className="icon" />
-                    <span className="text">More Info</span>
+                <div className="options">
+                    <div>
+                        <PlayArrowOutlined className="icon" />
+                        <span className="text">Play</span>
+                    </div>
+                    <div>
+                        <InfoOutlined className="icon" />
+                        <span className="text">More Info</span>
+                    </div>
                 </div>
             </div>
         </div>
