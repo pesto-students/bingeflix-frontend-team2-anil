b14263cbbd904791edcb6cd1fdc8aad185af573b
payas-kumar <bhrigu.payas10@gmail.com>
2022-10-28 20:33:28 +0530

    Fetch APIs

--- src/Components/ListItem/ListItem.jsx
+++ src/Components/ListItem/ListItem.jsx
@@ -5,25 +5,48 @@ import {
   ThumbUpAltOutlined,
   ThumbDownOutlined,
 } from "@material-ui/icons";
-import { useState } from "react";
+import { useState,useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
-const ListItem = ({index}) => {
+const ListItem = ({index, item}) => {
 
   const [isHovered, setIsHovered] = useState(false);
+  const [movie, setMovie ] = useState({});
+
+  useEffect(() => {
+    const getMovie = async ()=> {
+      try {
+        const res = await axios.get("/movies/find/" + item , {
+          headers: {
+            token:
+            "Bearer "+JSON.parse(localStorage.getItem("user")).accessToken,
+          },
+        });
+        setMovie(res.data)
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  getMovie()
+  }, [item])
   
 
   return (
+    <Link to={{pathname:"/watch",movie:movie}}>
     <div className="listItem"
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
       style={{ left: isHovered && index * 225 - 50 + index * 2.5 }}
       >
 
-      <img src={require("../../images/logo_1.png")} alt="" />
+      {/* <img src={require("../../images/logo_1.png")} alt="" /> */}
+      <img src={movie.img} alt="" />
 
       {isHovered && (
           <>
-            <video src={require('../../images/sample-mp4-file-small.mp4')} autoPlay={true} loop />
+            {/* <video src={require('../../images/sample-mp4-file-small.mp4')} autoPlay={true} loop /> */}
+            <video src={movie.trailer} autoPlay={true} loop />
 
             <div className="itemInfo">
 
@@ -35,21 +58,21 @@ const ListItem = ({index}) => {
               </div>
 
               <div className="itemInfoTop">
-                <span>1hr 32 mins</span>
-                <span className="limit">+16</span>
-                <span>2000</span>
+                <span>{movie.duration}</span>
+                <span className="limit">+{movie.limit}</span>
+                <span>{movie.year}</span>
               </div>
 
               <div className="desc">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Nunc ullamcorper tortor ut erat bibendum consectetur. 
+                {movie.desc} 
               </div>
 
-              <div className="genre">Action</div>
+              <div className="genre">{movie.genre}</div>
 
             </div>
           </>)}
     </div>
+    </Link>
   )
 }
 
--- src/pages/register/Register.js
+++ src/pages/register/Register.js
@@ -76,7 +76,7 @@ const Register = () => {
         </div>
         <div className='container'>
             <h1>Unlimited movies, TV shows, and more.</h1>
-            <h2>Watch anywhere. Canel anytime.</h2>
+            <h2>Watch anywhere. Cancel anytime.</h2>
             <p>
                 Ready to watch? Enter your email to create or restart your membership.
             </p>
